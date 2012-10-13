3b0762050af07a63cbb190d4e1a101ab159e370e
unknown <Uri@Yael-PC.(none)>
2012-10-13 22:57:15 +0200

    remove unused hooks

--- .openshift/action_hooks/miriagassi_db.new-users.sql
+++ .openshift/action_hooks/miriagassi_db.new-users.sql
@@ -3,6 +3,7 @@
 -- Host: localhost    Database: miriagassi_db
 -- ------------------------------------------------------
 -- Server version	5.1.61
+use 'graphology';
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
